6493ad4edeee3e48bbff21df153b81042b5b33e4
Sebastian Jachec <sjachec1@gmail.com>
2013-03-07 12:26:53 +0000

    Cleaned up some more, BOOL to only share on web

--- SJADNShareController.h
+++ SJADNShareController.h
@@ -7,6 +7,9 @@
 
 @interface SJADNShareController : NSObject <NSSharingServicePickerDelegate>
 
+//If YES, we try to use Mac app URL schemes
+@property BOOL *shareViaLocalApps;
+
 //Shares the given items on App.net
 - (void)shareItems:(NSArray*)items;
 
--- SJADNShareController.m
+++ SJADNShareController.m
@@ -7,6 +7,15 @@
 
 @implementation SJADNShareController
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _shareViaLocalApps = YES;
+    }
+    return self;
+}
+
 //Used to encode text for the App.net intent URL
 - (NSString*)encodeToPercentEscapeString:(NSString *)string {
     return (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
@@ -21,6 +30,7 @@
     
     NSMutableArray *sharingServices = [proposedServices mutableCopy];
     
+    //Dropdown menu image for ADN
     NSImage *ADNImage = [NSImage imageNamed:@"adn"];
     [ADNImage setTemplate:YES];
     
@@ -50,10 +60,40 @@
         }
     }
     
+    //Encode the post text, for URLs
     NSString *encodedPostText = [self encodeToPercentEscapeString:postText];
-    NSString *postURL = [NSString stringWithFormat:@"https://alpha.app.net/intent/post?text=%@",encodedPostText];
     
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:postURL]];
+    BOOL shared = NO;
+    if (_shareViaLocalApps) {
+        //Try to share with local apps
+        shared = [self shareWithApps:encodedPostText];
+        
+        //If unable to share with apps, share on web
+        if (!shared) [self shareWithWeb:encodedPostText];
+    } else {
+        //Share on web
+        shared = [self shareWithWeb:encodedPostText];
+    }
+}
+
+//Private: Share with any available apps
+- (BOOL)shareWithApps:(NSString*)encodedPostText {
+    NSString *kiwi = [[NSWorkspace sharedWorkspace] absolutePathForAppBundleWithIdentifier:@"com.yourhead.kiwi"];
+    NSString *postURL = nil;
+    if (kiwi) {
+        //Kiwi
+        postURL = [NSString stringWithFormat:@"kiwi://post?text=%@",encodedPostText];
+        return [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:postURL]];
+    } else {
+        //Nothing to open with
+        return NO;
+    }
+}
+
+//Private: Share using Alpha on Web
+- (BOOL)shareWithWeb:(NSString*)encodedPostText {
+    NSString *postURL = [NSString stringWithFormat:@"https://alpha.app.net/intent/post?text=%@",encodedPostText];
+    return [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:postURL]];
 }
 
 @end
